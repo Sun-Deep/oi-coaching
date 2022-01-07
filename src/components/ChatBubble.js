066d0c3fe7573f80fb85b5a102206dd02d1e5538
Sun-Deep <sundeepp29@gmail.com>
2022-01-07 17:50:23 +0545

    text to speech in chat bubble

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -1,7 +1,11 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Flex, Icon } from "@chakra-ui/react"
+import { BsVolumeDownFill } from "react-icons/bs";
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 export const ChatBubbleReceived = ({ text }) => {
-    return <Box
+    const { speak } = useSpeechSynthesis();
+    return <Flex gridGap={1} alignItems={'center'} mb={2}> 
+        <Box
         bg='rgb(78,166,86)'
         bg='linear-gradient(
             90deg, rgba(78,166,86,1) 0%, rgb(62 161 141 / 99%) 63%, rgb(5 121 145) 100%)'
@@ -14,10 +18,39 @@ export const ChatBubbleReceived = ({ text }) => {
     >
         {text}
     </Box>
+    <Box mt={1}>
+    <Icon 
+            alignSelf={'flex-end'} 
+            as={BsVolumeDownFill} 
+            cursor={'pointer'}
+            w={6}
+            h={6}
+            onClick={() => speak({ text: text })}
+            color={'gray'}
+           
+        />
+    </Box>
+    
+    </Flex>
 }
 
 export const ChatBubbleSent = ({ text }) => {
-    return <Box
+    const { speak } = useSpeechSynthesis();
+    return <Flex gridGap={1} alignSelf={'flex-end'} mb={2}>
+        <Box mt={1}>
+    <Icon 
+            alignSelf={'flex-end'} 
+            as={BsVolumeDownFill} 
+            cursor={'pointer'}
+            w={6}
+            h={6}
+            onClick={() => speak({ text: text })}
+            color={'gray'}
+            // mt={2}
+        />
+    </Box>
+
+    <Box
         bg='rgb(78,166,86)'
         bg='linear-gradient( 
             303deg, rgb(32 99 199) 0%, rgb(19 79 155 / 99%) 63%, rgb(32 89 100) 100%)'
@@ -27,8 +60,8 @@ export const ChatBubbleSent = ({ text }) => {
         boxShadow={'md'}
         borderRadius="25px"
         borderBottomRightRadius="0px"
-        alignSelf='flex-end'
     >
         {text}
     </Box>
+        </Flex>
 }
