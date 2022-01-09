c0619d9c0cf0cfd5e8a226a0632e69a0e56d7969
Sun-Deep <sundeepp29@gmail.com>
2022-01-09 14:31:12 +0545

    color constants

---
+++ src/constants/color.js
@@ -0,0 +1,18 @@
+export const COLOR = {
+    light: {
+        bg: '#EBEBEB',
+        color: '#000000'
+    },
+    dark: {
+        bg: '#03263A',
+        color: '#F1F1F1'
+    },
+    yellow: {
+        bg: '#FFEA00',
+        color: '#000000'
+    },
+    pink: {
+        bg: '#F979E6',
+        color: '#000000'
+    },
+}
